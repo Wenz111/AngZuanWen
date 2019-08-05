2ee58e152adc460d41308dc324f6f9854c63029d
taruc <angzw-wm17@student.tarc.edu.my>
2019-08-05 14:32:42 +0800

    Initial Commit

--- app/src/main/java/angzuanwen/com/my/UecActivity.java
+++ app/src/main/java/angzuanwen/com/my/UecActivity.java
@@ -3,15 +3,30 @@ package angzuanwen.com.my;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class UecActivity extends AppCompatActivity {
+public class UecActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String sub1;
+    private String sub2;
+    private String sub3;
+    private String sub4;
+    private String sub5;
+    private String sub6;
+    private String sub7;
+
+    private Button mBtnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_uec);
+        setContentView(R.layout.activity_spm);
 
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
@@ -25,6 +40,7 @@ public class UecActivity extends AppCompatActivity {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
@@ -32,6 +48,7 @@ public class UecActivity extends AppCompatActivity {
 
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
 
         Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
@@ -39,6 +56,7 @@ public class UecActivity extends AppCompatActivity {
 
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(this);
 
 
         Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
@@ -46,24 +64,90 @@ public class UecActivity extends AppCompatActivity {
                 R.array.grades_array, android.R.layout.simple_spinner_item);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner4.setAdapter(adapter4);
-
+        spinner4.setOnItemSelectedListener(this);
 
         Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
         ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
                 R.array.grades_array, android.R.layout.simple_spinner_item);
         adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner5.setAdapter(adapter5);
+        spinner5.setOnItemSelectedListener(this);
 
         Spinner spinner6 = (Spinner) findViewById(R.id.spinner6);
         ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,
                 R.array.grades_array, android.R.layout.simple_spinner_item);
         adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner6.setAdapter(adapter6);
+        spinner6.setOnItemSelectedListener(this);
 
         Spinner spinner7 = (Spinner) findViewById(R.id.spinner7);
         ArrayAdapter<CharSequence> adapter7 = ArrayAdapter.createFromResource(this,
                 R.array.grades_array, android.R.layout.simple_spinner_item);
         adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner7.setAdapter(adapter7);
+        spinner7.setOnItemSelectedListener(this);
+
+        mBtnCheck = findViewById(R.id.btnCheck);
+        mBtnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tx = findViewById(R.id.congratsText);
+                if (!sub1.equals("D") && !sub2.equals("E") && sub6.equals("B")){
+                    tx.setText(R.string.congrats);
+                } else {
+                    tx.setText(R.string.sorry);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        switch (parent.getId()) {
+            case R.id.spinner:
+                String text = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+                sub1 = text;
+                break;
+            case R.id.spinner2:
+                String text2 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text2, Toast.LENGTH_SHORT).show();
+                sub2 = text2;
+                break;
+            case R.id.spinner3:
+                String text3 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text3, Toast.LENGTH_SHORT).show();
+                sub3 = text3;
+                break;
+            case R.id.spinner4:
+                String text4 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text4, Toast.LENGTH_SHORT).show();
+                sub4 = text4;
+                break;
+            case R.id.spinner5:
+                String text5 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text5, Toast.LENGTH_SHORT).show();
+                sub5 = text5;
+                break;
+            case R.id.spinner6:
+                String text6 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text6, Toast.LENGTH_SHORT).show();
+                sub6 = text6;
+                break;
+            case R.id.spinner7:
+                String text7 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text7, Toast.LENGTH_SHORT).show();
+                sub7 = text7;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
